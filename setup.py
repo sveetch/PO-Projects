f989e71ba0d46ef3a9706a721a9d255b1c642820
David THENON <sveetch@gmail.com>
2013-11-07 18:00:02 +0100

    update setup.py for bad dependancy on crispy

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'django-braces==1.0.0',
         'South==0.7.6',
         'djangocodemirror==0.9.3',
-        'django-crispy-forms >= 1.2, <=1.3.2',
+        'django-crispy-forms>=1.2',
     ],
     include_package_data=True,
     zip_safe=False
