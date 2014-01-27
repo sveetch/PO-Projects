4c27f8e9aa8d8215b541938da4e89f7a1e77a792
David THENON <sveetch@gmail.com>
2014-01-27 01:56:14 +0100

    Add autobreadcrumbs requirement and usage, bumping version

---
+++ po_projects/crumbs.py
@@ -0,0 +1,13 @@
+from autobreadcrumbs import site
+from django.utils.translation import ugettext_lazy
+
+site.update({
+    'project-index': ugettext_lazy('PO Projects'),
+    'project-create': ugettext_lazy('Create a new project'),
+    'project-details': ugettext_lazy('Project'),
+    'project-update': ugettext_lazy('Project update'),
+    'project-download': None,
+    'catalog-details': ugettext_lazy('Catalog'),
+    'catalog-messages-edit': ugettext_lazy('Edit messages'),
+    'catalog-messages-download': None,
+})
