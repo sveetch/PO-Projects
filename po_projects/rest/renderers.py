3d03d99db01cf7e5d7c8a0578c19b0991cdc6a01
David THENON <sveetch@gmail.com>
2014-04-06 18:47:57 +0200

    Minor fixes for ressource hyperlinks, better project versions list, add some endpoint docs with markdown

--- po_projects/rest/renderers.py
+++ po_projects/rest/renderers.py
@@ -1,11 +1,11 @@
-from django.utils.encoding import smart_unicode
-from rest_framework import renderers
+#from django.utils.encoding import smart_unicode
+#from rest_framework import renderers
 
-class TarballRenderer(renderers.BaseRenderer):
-    media_type = 'application/x-gzip'
-    format = 'gz'
-    charset = None
-    render_style = 'binary'
+#class TarballRenderer(renderers.BaseRenderer):
+    #media_type = 'application/x-gzip'
+    #format = 'gz'
+    #charset = None
+    #render_style = 'binary'
 
-    def render(self, data, media_type=None, renderer_context=None):
-        return data
+    #def render(self, data, media_type=None, renderer_context=None):
+        #return data
